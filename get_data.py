b6b1211327c446e86f168ab2487803d644405881
Wayne-dot <wayneting.wc@gmail.com>
2023-06-11 08:33:23 -0400

    changes

--- get_data.py
+++ get_data.py
@@ -1,10 +1,22 @@
 import requests
 import cv2
+from bs4 import BeautifulSoup
 
 # Issue - image too big
 # maximum (700, 750)
 
 URL = "https://leveling-solo.net/manga/the-solo-leveling-chapter-1/"
+
+# separator
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+pictures_list = soup.find_all("div", "separator")
+
+for div in pictures_list:
+    link = div.find("a")
+    print(link["href"])
+    break
 
 image = cv2.imread("data/file1.jpg")
 height = image.shape[0]
