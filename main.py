c851e29ec9ac3dd0aa82267b967c940f8f0a02c1
Wayne-dot <wayneting.wc@gmail.com>
2023-06-11 02:13:55 -0400

    changes

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-import cv2
+import cv2 as cv
 
+# draw a rectangle bounding box with mouse
+
+def draw_rectange(event, x, y, flags, param):
+    
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        print("left is press")
+
+    if event == cv.EVENT_LBUTTONUP:
+        print("left is release")
+
+
+
+    # cv.rectangle()
+    # cv.imshow()
+
+
+image = cv.imread("data/file1.jpg")
+cv.imshow("image", image)
+cv.setMouseCallback("image", draw_rectange)
+cv.waitKey(0)
+cv.destroyAllWindows()
