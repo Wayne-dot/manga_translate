b807165b2b3ee82fa3624403b341a71e001c59b3
Wayne-dot <wayneting.wc@gmail.com>
2023-06-11 10:23:37 -0400

    change

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 
+# write documentation
+
 # draw a rectangle bounding box with mouse
 
 class draw_bounding_box:
@@ -7,6 +9,8 @@ class draw_bounding_box:
         self.corrdinate = []
         self.start = ()
         self.image = None
+        self.height = None
+        self.width = None
 
     def draw_rectange(self, event, x, y, flags, param):
 
@@ -18,14 +22,23 @@ class draw_bounding_box:
             end = (x, y)
             self.corrdinate.append(end)
             # print(end)
-
-        
             cv.rectangle(self.image, self.start, end, (0, 255, 0), 2)
             cv.imshow("image", self.image)
     
     def run(self, image_path):
         self.image = cv.imread(image_path)
+        # convert to grey image
+        grey = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         cv.imshow("image", self.image)
+        self.height = self.image.shape[0]
+        self.width = self.image.shape[1]
+
+        th2 = cv.adaptiveThreshold(grey, 255, cv.THRESH_BINARY_INV, cv.ADAPTIVE_THRESH_GAUSSIAN_C, 101, 10)
+        
+        cv.imshow("adopte", th2)
+
+
+
         cv.setMouseCallback("image", self.draw_rectange)
         cv.waitKey(0)
         cv.destroyAllWindows()
@@ -33,8 +46,10 @@ class draw_bounding_box:
 
 
 bounding_box = draw_bounding_box()
-bounding_box.run("data/file1.jpg")
+bounding_box.run("data/chapter-1_page3.jpg")
 print(bounding_box.corrdinate)
+print(bounding_box.height)
+print(bounding_box.width)
 
 # process image
 # # Issue - image too big
