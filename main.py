2b4ad711741cf4b484b13f701787025f5953e2fb
Wayne-dot <wayneting.wc@gmail.com>
2023-06-11 02:21:40 -0400

    changes

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import cv2 as cv
 # draw a rectangle bounding box with mouse
 
 def draw_rectange(event, x, y, flags, param):
-    
+
+    list_cor = []
 
     if event == cv.EVENT_LBUTTONDOWN:
-        print("left is press")
+        list_cor.append(x)
+        list_cor.append(y)
 
     if event == cv.EVENT_LBUTTONUP:
-        print("left is release")
+        list_cor.append(x)
+        list_cor.append(y)
+        end = (x, y)
+        print(end)
 
-
-
-    # cv.rectangle()
-    # cv.imshow()
+    
+        cv.rectangle(image, (list_cor[0], list_cor[1]), (list_cor[2], list_cor[3]), (0, 255, 0), 2)
+        cv.imshow("image", image)
 
 
 image = cv.imread("data/file1.jpg")
